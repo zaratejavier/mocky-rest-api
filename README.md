593845638807217b075bb3b24690c7d5f1388775
Ruy Garcia <rrgarciach@gmail.com>
2021-06-03 01:21:32 -0600

    Added README, users endpoint, authentication and validations

---
+++ README.md
@@ -0,0 +1,59 @@
+# Mocky REST API
+
+This is a simple REST API mock server for testing/sampling frontend applications.
+
+### System Requirements:
+- NodeJS
+
+### Setup
+
+1. `npm install`
+1. `npm start`
+
+### Authentication
+
+`POST /api/v1/auth`
+```json
+{
+    "email": "johnd@example.com",
+    "password": "asdf123!"
+}
+```
+
+Once authenticated, use Authorization header to include provided token.
+
+### Endpoints
+
+#### Items
+- `GET /api/v1/items`
+- `GET /api/v1/items/:id`
+- `POST /api/v1/items`
+- `PUT /api/v1/items/:id`
+- `DELETE /api/v1/items/:id`
+
+```json
+{
+    "title": "yolo",
+    "body": "text here",
+    "date": "2020-12-12"
+}
+```
+
+#### Users (admin access only)
+- `GET /api/v1/users`
+- `GET /api/v1/users/:id`
+
+```json
+{
+    "email": "johnd@example.com",
+    "password": "asdf123!",
+    "role": "admin"
+}
+```
+```json
+{
+    "email": "janed@example.com",
+    "password": "qwer1234!",
+    "role": "user"
+}
+```
--- index.js
+++ index.js
@@ -1,47 +1,105 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const moment = require('moment');
+
+const V1 = '/api/v1';
 
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-let server = app.listen(3000, () => {
+let server = app.listen(9090, () => {
  console.log('server is running on port', server.address().port);
 });
 
+const users = [
+   {
+      email: 'johnd@example.com',
+      password: 'asdf123!',
+      role: 'admin'
+   },
+   {
+      email: 'janed@example.com',
+      password: 'qwer1234!',
+      role: 'user'
+   }
+];
 let items = [];
 
-app.get('/items', (req, res) => {
+app.post(V1 + '/auth', (req, res) => {
+   const {email, password} = req.body;
+   if (!email || !password) return res.sendStatus(401);
+   let user = users.find(u => u.email === email);
+   if (!user) return res.sendStatus(401);
+   if (user.password !== password) return res.sendStatus(401);
+   delete user.password;
+   res.send({ token: escape(JSON.stringify(user)) });
+});
+
+app.get(V1 + '/users', authenticate, authorize, (req, res) => {
+   res.send(users.map((u, id) => ({id, ...u})));
+});
+
+app.get(V1 + '/users/:id', authenticate, authorize, (req, res) => {
+   const id = req.params.id;
+   if (!users[id]) return res.sendStatus(404);
+   res.send({id, ...users[id]});
+});
+
+app.get(V1 + '/items', authenticate, (req, res) => {
    res.send(items.map((i, id) => ({id, ...i})));
 });
 
-app.get('/items/:id', (req, res) => {
+app.get(V1 + '/items/:id', authenticate, (req, res) => {
    const id = req.params.id;
    if (!items[id]) return res.sendStatus(404);
    res.send({id, ...items[id]});
 });
 
-app.post('/items', (req, res) => {
+app.post(V1 + '/items', authenticate, (req, res) => {
    const {title, body, date} = req.body;
-   if (!title || !body || !date) return res.sendStatus(404);
+   if (!validateItem(req.body)) return res.sendStatus(400);
    const item = {title, body, date};
    items.push(item);
    res.send(item);
 });
 
-app.put('/items/:id', (req, res) => {
+app.put(V1 + '/items/:id', authenticate, (req, res) => {
    const id = req.params.id;
    if (!items[id]) return res.sendStatus(404);
    const {title, body, date} = req.body;
-   if (!title || !body || !date) return res.sendStatus(404);
+   if (!validateItem(req.body)) return res.sendStatus(400);
    items[id] = {title, body, date};
    res.send({id, ...items[id]});
 });
 
-app.delete('/items/:id', (req, res) => {
+app.delete(V1 + '/items/:id', authenticate, (req, res) => {
    const id = req.params.id;
    if (!items[id]) return res.sendStatus(404);
    delete items[id];
    res.send();
 });
+
+function validateItem(data = {}) {
+   if (!data.title || !data.body || !data.date) return false;
+   const date = moment(data.date, 'YYYY-MM-DD', true);
+   return date.isValid();
+}
+
+function authenticate(req, res, next) {
+   const token = req.headers['authorization'];
+   if (!token) return res.send(401);
+   try {
+      req.user = JSON.parse(unescape(token));
+   } catch (err) {
+      console.error(err);
+      return res.send(401);
+   }
+   next();
+}
+
+function authorize(req, res, next) {
+   if (req.user.role !== 'admin') return res.send(403);
+   next();
+}
